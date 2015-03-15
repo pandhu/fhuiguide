63b0e03cfd732e6c7644673e0fb62e21d9b4d16c
tsaqibb <saqib.abud@gmail.com>
2015-03-15 23:43:19 +0700

    marge admincontroller

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -103,4 +103,130 @@ class AdminController extends Controller{
 		$this->redirect('posts/');
 	}
 }
+/**------------RANCANGAN KULIAH---------------**/
+	public function actionRancanganKuliah(){
+		$this -> layout = 'main';
+		$peminatan = Peminatan::model() -> findAll();
+		$matkul_wajib = MataKuliahWajib::model() -> findAll();
+		$this -> render('/admin/rancangan_kuliah_list',
+			array('peminatan' => $peminatan, 'matkul_wajib'=>$matkul_wajib));
+	}
+
+	/**--------PEMINATAN----------**/
+	public function actionEditPeminatan($id){
+		$models = Peminatan::model()->findByPk($id);
+		$this -> render('/admin/peminatan_edit', array('models' => $models, 'id'=>$id));
+	}
+
+	public function actionSaveEditPeminatan($id){
+		$peminatan = Peminatan::model()->findByPk($id);
+		$peminatan->nama = $_POST['Peminatan']['nama'];
+		$peminatan->rancangan_kuliah = $_POST['Peminatan']['rancangan_kuliah'];
+		$peminatan->update();
+		$this->redirect('../editpeminatan/'.$id);
+	}
+
+	public function actionAddPeminatan(){
+		$models = Peminatan::model();	
+		$this -> render('/admin/peminatan_add', array('models' => $models,));
+	}
+
+	public function actionSavePeminatan(){
+		$peminatan = new Peminatan();
+		$peminatan->nama = $_POST['Peminatan']['nama'];
+		$peminatan->rancangan_kuliah = $_POST['Peminatan']['rancangan_kuliah'];
+		$peminatan->save(false);
+		$this->redirect('/admin/rancangankuliah/');
+	}
+
+	/**--------MATKUL WAJIB----------*/
+	public function actionEditMatkulWajib($id){
+		$models = MataKuliahWajib::model()->findByPk($id);
+		$this -> render('/admin/matkul_wajib_edit', array('models' => $models, 'semester'=>$id));
+	}
+	
+	public function actionSaveEditMatkulWajib($id){
+		$matkul_wajib = MataKuliahWajib::model()->findByPk($id);
+		$matkul_wajib->rancangan_kuliah = $_POST['MataKuliahWajib']['rancangan_kuliah'];
+		$matkul_wajib->update();
+		$this->redirect('/admin/rancangankuliah/');
+	}
+
+/**-------------------------------MATKUL----------------------------------**/	
+	public function actionMatkul(){
+		$this -> layout = 'main';
+		$peminatan = Peminatan::model() -> findAll();
+		$matkul_wajib = MataKuliahWajib::model() -> findAll();
+		$this -> render('/admin/matkul_list',
+			array('peminatan' => $peminatan, 'matkul_wajib'=>$matkul_wajib));
+	}
+
+	public function actionEditMatkul($id){
+		$models = MataKuliah::model()->findByPk($id);
+		$this -> render('/admin/matkul_edit', array('models' => $models, 'id' => $id));
+	}
+
+	public function actionSaveEditMatkul($id){
+		$matkul = MataKuliah::model()->findByPk($id);
+		$matkul->nama = $_POST['MataKuliah']['nama'];
+		$matkul->update();
+		$this->redirect('../editMatkul/'.$id);
+	}
+
+	public function actionAddMatkulPeminatan($id){
+		$models = MataKuliah::model();
+		$this -> render('/admin/matkul_add',
+			array('models' => $models, 'peminatan_id' => $id));
+	}
+
+	public function actionSaveMatkulPeminatan($id){
+		$matkul = new MataKuliah();
+		$matkul->nama = $_POST['MataKuliah']['nama'];
+		$matkul->peminatan_id = $id;
+		$matkul->save(false);
+		$this->redirect('/admin/Matkul/');
+	}
+	
+	public function actionAddMatkul($id){
+		$models = MataKuliah::model();
+		$this -> render('/admin/matkul_add',
+			array('models' => $models, 'matkul_wajib_id' => $id ));
+	}
+
+	public function actionSaveMatkul($id){
+		$matkul = new MataKuliah();
+		$matkul->nama = $_POST['MataKuliah']['nama'];
+		$matkul->matkul_wajib_id = $id;
+		$matkul->save(false);
+		$this->redirect('/admin/Matkul/');
+	}
+
+	/**-------BAHAN KULIAH--------*/
+	public function actionBahanKuliah(){
+		$this -> layout = 'main';
+		$peminatan = Peminatan::model() -> findAll();
+		$matkul_wajib = MataKuliahWajib::model() -> findAll();
+		$this -> render('/admin/bahan_kuliah_list',
+			array('peminatan' => $peminatan, 'matkul_wajib'=>$matkul_wajib));
+	}
+
+	/**-------DIKTAT--------*/
+	public function actionDiktat(){
+		$this -> layout = 'main';
+		$peminatan = Peminatan::model() -> findAll();
+		$matkul_wajib = MataKuliahWajib::model() -> findAll();
+		$this -> render('/admin/bahan_kuliah_list',
+			array('peminatan' => $peminatan, 'matkul_wajib'=>$matkul_wajib));
+	}
+	
+	/**-------BANK SOAL--------*/
+	public function actionBankSoal(){
+		$this -> layout = 'main';
+		$peminatan = Peminatan::model() -> findAll();
+		$matkul_wajib = MataKuliahWajib::model() -> findAll();
+		$this -> render('/admin/soal_list',
+			array('peminatan' => $peminatan, 'matkul_wajib'=>$matkul_wajib));
+	}
+}
+?>
 ?>
