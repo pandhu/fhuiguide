1d6ab6b02853aa8c94b291409ab935093dd98a0d
pandhu <pandhu.hutomo@ui.ac.id>
2015-03-19 12:18:44 +0700

    home sementara

--- protected/controllers/BerandaController.php
+++ protected/controllers/BerandaController.php
@@ -4,6 +4,8 @@ class BerandaController extends Controller
 {
 	public function actionIndex()
 	{
+		$this->layout="home";
+
 		// renders the view file 'protected/views/site/beranda.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('/site/beranda');
